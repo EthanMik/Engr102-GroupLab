74a26810fe145c1349fe42d3d4979a3be5aa364a
Ruben <rubenmartinez90@tamu.edu>
2025-10-18 01:34:08 -0500

    Printed clock

--- ascii_clock.py
+++ ascii_clock.py
@@ -4,11 +4,11 @@ glyphs = {
         "101",
         "101",
         "111"],
-    "1": ["110",
-        "010",
-        "010",
-        "010",
-        "111"],
+    "1": ["010",
+          "110",
+          "010",
+          "010",
+          "111"],
     "2": ["111",
         "001",
         "001",
@@ -70,56 +70,24 @@ glyphs = {
         "010",
         "000"],
 }
+def print_clock():
+
+    time = "12:57"
+    
+    for i in range(5):
+        for char in time:
+            new_string = ""
+            for s in glyphs[char][i]:
+                if s == "0":
+                    new_string += " "
+                
+                if s != "0":
+                    new_string += s
+            print (new_string, end=" ")
+        print()
 
 def main():
-
-    if glyphs == "0":
-        print (glyphs["0"])
-
-    if glyphs == "1":
-        print (glyphs["1"])
-
-    if glyphs == "2":
-        print (glyphs["2"])
-
-    if glyphs == "3":
-        print (glyphs["3"])
-
-    if glyphs == "4":
-        print (glyphs["4"])
-
-    if glyphs == "5":
-        print (glyphs["5"])\
-            
-    if glyphs == "6":
-        print (glyphs["6"])
-
-    if glyphs == "7":
-        print (glyphs["7"])
-
-    if glyphs == "7":
-        print (glyphs["7"])
-
-    if glyphs == "8":
-        print (glyphs["8"])
-            
-    if glyphs == "9":
-        print (glyphs["9"])
-        
-    if glyphs == "A":
-        print (glyphs["A"])
-
-    if glyphs == "P":
-        print (glyphs["P"])
-
-    if glyphs == "M":
-        print (glyphs["M"])
-        
-    if glyphs == ":":
-        print (glyphs[":"])
-
-
-
+    pass
 
 if __name__ == "__main__":
     main()
